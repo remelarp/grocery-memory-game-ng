a176813c33a1c4f67e923ee07eb706e5f6ef2764
Remelar Panganiban <remelarp@gmail.com>
2025-10-25 11:31:52 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'grocery-memory-app',
+  webDir: 'www'
+};
+
+export default config;
